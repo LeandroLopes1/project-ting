181ecd3cdee02370d832cc1af8f39957cb02cc1c
Leandro Lopes <leandro_carlos@uol.com.br>
2022-03-13 11:57:40 -0300

    req 02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file.endswith(".txt"):
+
+        try:
+            with open(path_file, "r") as file:
+                text = file.read()
+                return text.split("\n")
+
+        except FileNotFoundError:
+            sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+    else:
+        sys.stderr.write("Formato inválido\n")
